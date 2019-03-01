26d3676e083491c383d8a1275c02bd272ee7e9b3
QAP12 <QAP12@QAP12-PC>
2019-03-01 12:16:33 +0530

    commit3456

--- src/sony12/sony1234.java
+++ src/sony12/sony1234.java
@@ -1,5 +1,33 @@
 package sony12;
 
-public class sony1234 {
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+public class sony1234{
+	@BeforeClass
+	public static void login()
+	{
+		System.out.println("Logging In...");
+	}
+	@AfterClass
+	public static void logout()
+	{
+		System.out.println("Logging out...");
+	}
+	@Test
+	public void method3()
+	{
+		
+		Assert.assertEquals(11, 11);
+		System.out.println("Method3");
+	}
+	@Test
+	public void method4()
+	{
+		Assert.assertEquals(11, 11);
+		System.out.println("Method4");
+	}
 }
+
